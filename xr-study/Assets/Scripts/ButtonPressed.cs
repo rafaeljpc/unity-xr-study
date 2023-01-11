43cd8ed27fb5900b3b11f82436571a94d2e3b090
Rafael Correia <rafaeljpc@gmail.com>
2023-01-11 12:59:45 -0300

    UI Test

---
+++ xr-study/Assets/Scripts/ButtonPressed.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPressed : MonoBehaviour {
+
+    // public void Start() {
+    //     var button = this.GetComponent<Button>();
+    //     button.onClick.AddListener(() => this.OnClick());
+    // }
+
+    public void OnClick() {
+        Debug.Log("Apertou");
+    }
+
+}
